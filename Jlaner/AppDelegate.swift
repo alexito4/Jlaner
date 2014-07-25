1781ab7362665219f961359a58af13ab097379af
Alejandro Martinez <alexito4@gmail.com>
2014-07-25 20:25:43 +0100

    Initial Commit

---
+++ Jlaner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Jlaner
+//
+//  Created by Alejandro Martinez on 25/07/14.
+//  Copyright (c) 2014 Alejandro Martinez. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Jlaner/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Jlaner
+//
+//  Created by Alejandro Martinez on 25/07/14.
+//  Copyright (c) 2014 Alejandro Martinez. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
